9bfecea56ac1a09c5d37a1603d7a122b78594368
Boris Aguilar <baguilar@innovare.es>
2021-10-12 18:55:23 -0600

    First commit

---
+++ 14-std-do/dockerbase.tf
@@ -0,0 +1,70 @@
+variable "database_password" {
+    default = ""
+}
+
+variable "domain" {
+    default = ""
+}
+
+resource "random_password" "database_password" {
+  length           = 16
+  special          = true
+  override_special = "_%@"
+}
+
+resource "digitalocean_droplet" "www-odoo" {
+  image = "ubuntu-20-04-x64"
+  name = "www-1"
+  region = "nyc3"
+  size = "s-1vcpu-1gb"
+  ssh_keys = [
+    digitalocean_ssh_key.terraform.id
+  ]
+
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = var.pvt_key != "" ? file(var.pvt_key) : tls_private_key.pk.private_key_pem
+    timeout = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      # install nginx
+      "sudo apt update",
+      "sudo apt install -y nginx",
+      "sudo apt install -y docker",
+      "sudo apt install -y docker-compose",
+      "sudo apt install -y python3-certbot-nginx",
+      # create odoo installation directory
+      "mkdir /root/odoo",
+      "mkdir /root/odoo/addons",
+      "mkdir /root/odoo/config"
+    ]
+  }
+
+  provisioner "file" {
+    content      = templatefile("docker-compose.yml.tpl", {
+      database_password = var.database_password != "" ? var.database_password : random_password.database_password.result
+    })
+    destination = "/root/odoo/docker-compose.yml"
+  }
+
+  provisioner "file" {
+    content      = templatefile("atmosphere-nginx.conf.tpl", {
+      server_name = var.domain != "" ? var.domain : "0.0.0.0"
+    })
+    destination = "/etc/nginx/conf.d/atmosphere-nginx.conf"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      # run compose
+      "cd /root/odoo",
+      "docker-compose up -d"
+    ]
+  }
+}
---
+++ 14-std-do/provider.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+  provisioner "local-exec" { # Create "myKey.pem" to your computer!!
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ./myKey"
+  }
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.pk.public_key_openssh}' > ./myKey.pub"
+  }
+}
+
+variable "do_token" {}
+variable "pvt_key" {
+    default = "" #tls_private_key.pk.private_key_pem
+}
+variable "pub_key" {
+    default = "" #tls_private_key.pk.public_key_openssh
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_ssh_key" "terraform" {
+  name = "terraform"
+  public_key = var.pub_key != "" ? file(var.pub_key) : tls_private_key.pk.public_key_openssh
+}
